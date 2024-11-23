8a1d038f3a870a2c3c5570ed6b0fa8448a755dc4
sthasaroj13 <sarojshrestha651@gmail.com>
2024-11-23 16:46:44 +0545

    Add project files.

---
+++ FoundationCSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FoundationCSharp
+{
+    internal class Program
+    {
+        class BankAmmount
+        {
+
+            private double balance;
+
+            public void Deposit(double amt)
+            {
+                if (amt > 0)
+                {
+                    balance += amt;
+                    Console.WriteLine($"current amt:{amt}. The new balance is:{balance}");
+
+                }
+                else
+                {
+                    Console.WriteLine("the ammount must be postive");
+                }
+
+
+            }
+            public double GetBalance()
+            {
+                return balance;
+            }
+        }
+
+       
+
+
+        static void Main(string[] args)
+        {
+            BankAmmount bank = new BankAmmount();
+            bank.Deposit(500);
+
+            Console.WriteLine($"Your total  balance is {bank.GetBalance()}");
+        }
+    }
+}
